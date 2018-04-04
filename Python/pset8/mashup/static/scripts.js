f94e72673d7d460bf41ff9fa3d1d4ee521a75e29
Ghiath Alshammaa <ghiathalshammaa@MacBook-Pro-4.local>
2018-04-04 15:34:26 +0200

    mashup is ready

---
+++ Python/pset8/mashup/static/scripts.js
@@ -0,0 +1,263 @@
+// Google Map
+let map;
+
+// Markers for map
+let markers = [];
+
+// Info window
+let info = new google.maps.InfoWindow();
+
+
+// Execute when the DOM is fully loaded
+$(document).ready(function() {
+
+    // Styles for map
+    // https://developers.google.com/maps/documentation/javascript/styling
+    let styles = [
+
+        // Hide Google's labels
+        {
+            featureType: "all",
+            elementType: "labels",
+            stylers: [
+                {visibility: "off"}
+            ]
+        },
+
+        // Hide roads
+        {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [
+                {visibility: "off"}
+            ]
+        }
+
+    ];
+
+    // Options for map
+    // https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    let options = {
+        //center: {lat: 48.2167, lng: 16.4}, // Stanford, California
+        center: {lat: 37.4236, lng: -122.1619},
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        maxZoom: 14,
+        panControl: true,
+        styles: styles,
+        zoom: 13,
+        zoomControl: true
+    };
+
+    // Get DOM node in which map will be instantiated
+    let canvas = $("#map-canvas").get(0);
+
+    // Instantiate map
+    map = new google.maps.Map(canvas, options);
+
+    // Configure UI once Google Map is idle (i.e., loaded)
+    google.maps.event.addListenerOnce(map, "idle", configure);
+
+});
+
+
+// Add marker for place to map
+function addMarker(place)
+{
+    // TODO
+
+    // some varibles, I need them later
+    var lat=place.latitude, long=place.longitude, place_title=place.place_name;
+    var icon = "http://maps.gstatic.com/mapfiles/ms2/micons/ylw-pushpin.png";
+
+    // determine the place coordinates
+    var myLatlng = new google.maps.LatLng(lat,long);
+
+
+    // create a Marker
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: place_title,
+        label: place_title,
+        animation: google.maps.Animation.DROP,
+        icon: icon
+    });
+
+    // let the marker listen to a click
+    google.maps.event.addListener(marker,'click',function () {
+
+        // create a parameter contents geo and this is variable came with the link
+        let parameters = {
+        geo: place.postal_code
+        };
+
+        $.getJSON("/articles", parameters, function(data, jqXHR){
+            var articles_box = "<ul>";
+            for (var i = 0; i < data.length; i++) {
+                // create a html code contents the articles as links
+                articles_box += "<li>";
+                articles_box += "<a href='" + data[i].link + "'>";
+                articles_box += data[i].title;
+                articles_box += "</a>";
+                articles_box += "</li>";
+            }
+            articles_box += "</ul>";
+            showInfo(marker, articles_box);
+        });
+
+    });
+
+    // set the markers on the map
+    markers.push(marker);
+}
+
+
+// Configure application
+function configure()
+{
+    // Update UI after map has been dragged
+    google.maps.event.addListener(map, "dragend", function() {
+
+        // If info window isn't open
+        // http://stackoverflow.com/a/12410385
+        if (!info.getMap || !info.getMap())
+        {
+            update();
+        }
+    });
+
+    // Update UI after zoom level changes
+    google.maps.event.addListener(map, "zoom_changed", function() {
+        update();
+    });
+
+    // Configure typeahead
+    $("#q").typeahead({
+        highlight: false,
+        minLength: 1
+    },
+    {
+        display: function(suggestion) { return null; },
+        limit: 10,
+        source: search,
+        templates: {
+            suggestion: Handlebars.compile(
+                "<div>" +
+                "{{place_name}}, {{admin_name1}}, {{postal_code}}" +
+                "</div>"
+            )
+        }
+    });
+
+    // Re-center map after place is selected from drop-down
+    $("#q").on("typeahead:selected", function(eventObject, suggestion, name) {
+
+        // Set map's center
+        map.setCenter({lat: parseFloat(suggestion.latitude), lng: parseFloat(suggestion.longitude)});
+
+        // Update UI
+        update();
+    });
+
+    // Hide info window when text box has focus
+    $("#q").focus(function(eventData) {
+        info.close();
+    });
+
+    // Re-enable ctrl- and right-clicking (and thus Inspect Element) on Google Map
+    // https://chrome.google.com/webstore/detail/allow-right-click/hompjdfbfmmmgflfjdlnkohcplmboaeo?hl=en
+    document.addEventListener("contextmenu", function(event) {
+        event.returnValue = true;
+        event.stopPropagation && event.stopPropagation();
+        event.cancelBubble && event.cancelBubble();
+    }, true);
+
+    // Update UI
+    update();
+
+    // Give focus to text box
+    $("#q").focus();
+}
+
+
+// Remove markers from map
+function removeMarkers()
+{
+    // TODO
+    // remove all markers from the map
+    markers.forEach(function(marker)
+    {
+        marker.setMap(null);
+    });
+    markers = [];
+}
+
+
+// Search database for typeahead's suggestions
+function search(query, syncResults, asyncResults)
+{
+    // Get places matching query (asynchronously)
+    let parameters = {
+        q: query
+    };
+    $.getJSON("/search", parameters, function(data, textStatus, jqXHR) {
+        asyncResults(data);
+    })
+
+
+}
+
+
+// Show info window at marker with content
+function showInfo(marker, content)
+{
+    // Start div
+    let div = "<div id='info'>";
+    if (typeof(content) == "undefined")
+    {
+        // http://www.ajaxload.info/
+        div += "<img alt='loading' src='/static/ajax-loader.gif'/>";
+    }
+    else
+    {
+        div += content;
+    }
+
+    // End div
+    div += "</div>";
+
+    // Set info window's content
+    info.setContent(div);
+
+    // Open info window (if not already open)
+    info.open(map, marker);
+}
+
+
+// Update UI's markers
+function update()
+{
+    // Get map's bounds
+    let bounds = map.getBounds();
+    let ne = bounds.getNorthEast();
+    let sw = bounds.getSouthWest();
+
+    // Get places within bounds (asynchronously)
+    let parameters = {
+        ne: `${ne.lat()},${ne.lng()}`,
+        q: $("#q").val(),
+        sw: `${sw.lat()},${sw.lng()}`
+    };
+    $.getJSON("/update", parameters, function(data, textStatus, jqXHR) {
+
+       // Remove old markers from map
+       removeMarkers();
+
+       // Add new markers to map
+       for (let i = 0; i < data.length; i++)
+       {
+           addMarker(data[i]);
+       }
+    });
+};
