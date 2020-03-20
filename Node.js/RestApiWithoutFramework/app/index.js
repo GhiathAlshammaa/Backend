e27519148d03a64351640923cea43f5c7d534472
Ghiath2017 <g.alshamaa@hotmail.com>
2020-03-20 16:54:10 +0100

    Parsing Request Paths

--- Node.js/RestApiWithoutFramework/app/index.js
+++ Node.js/RestApiWithoutFramework/app/index.js
@@ -5,11 +5,25 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 const port = 3000;
 
 // The Server should respond to all requests with a string
 const server = http.createServer((req, res) => {
-    console.log('Hello World\n');
+
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+    // Send the response 
+    res.end('Hello World\n');
+
+    // Log the request path
+    console.log('Request recived on path:', trimmedPath);
+
 });
 
 // Start the server, and have it listen on port 3000
